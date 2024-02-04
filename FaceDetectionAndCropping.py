5caca117334ace9c3c7eb38f7f16189deee93d3b
Koushik Karan Geetha Nagaraj <116392599+KoushikKaranGeethaNagaraj@users.noreply.github.com>
2024-02-04 10:27:22 -0500

    Update FaceDetectionAndCropping.py

--- FaceDetectionAndCropping.py
+++ FaceDetectionAndCropping.py
@@ -12,7 +12,7 @@ class Image:
     def __init__(self):
         pass
     def annotate_camera_image(self,robot, event_type,event2, done):
-        # get image with face annotations
+        # get an image with face annotations
         annotate_PIL_image=event2.image.annotate_image()
         annotated_open_cv_image=np.array(annotate_PIL_image)
         annotated_open_cv_image=annotated_open_cv_image[:,:,::-1].copy()
@@ -39,17 +39,11 @@ class Image:
         gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
         gray = cv2.bilateralFilter(gray, 11, 17, 17)
         
-
         # dilation = cv2.dilate(erosion,kernel,iterations = 2)
         edged = cv2.Canny(gray, 50, 200)
-
-
         contours, hierarchy = cv2.findContours(edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
         rects = [cv2.boundingRect(cnt) for cnt in contours]
         rects = sorted(rects,key=lambda  x:x[1],reverse=True)
-
-
         i = -1
         j = 1
         y_old = 5000
@@ -64,35 +58,28 @@ class Image:
             rect=rects[max_element_index]
             area=max(area_list)         
             print(area)
+            #cropping per specified area
             if area > 5000 and area < 70000:
-
                 if (y_old - y) > 200:
                     i += 1
                     y_old = y
-
                 if abs(x_old - x) > 300:
                     x_old = x
                     x,y,w,h = rect
-
                     out = open_cv_primary_image[y-10:y+h+5,x+20:x+w]
                     j+=1
             if len(out)!=0:
-                cv2.imshow("Hi",out)
+                cv2.imshow("Output",out)
                 cv2.waitKey(0)
-
-
-
-
-        i=0
+                
+        #saving image that is seen for reference this can be used to make datasets
         directory=r'C:\Anki_vector'
         cv2.imwrite(str(i)+'cropped_img.jpg',self.annotate)
         cv2.imwrite(str(i)+'normal.jpg', open_cv_primary_image)
         os.listdir(directory)
-        i+=1
-        cv2.imshow("Hi",)
-        cv2.waitKey(0)
         done.set()
 
+# This function looks if there is any face and implements gesture detection
 with anki_vector.Robot(enable_face_detection=True, enable_custom_object_detection=True) as robot:
     robot.camera.init_camera_feed()
     done = threading.Event()
